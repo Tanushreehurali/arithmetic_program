4c5c14a1004afc32d2eee8333fff7e9e1d09acab
Tanushreehurali <tanuhurali@gmail.com>
2025-10-23 21:22:42 +0530

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+def add (a,b):
+  return a+b
+def subtract (a,b):
+  return a-b
+  def multiply (a,b):
+    return a*b
+    def divide (a.b):
+      if b==0
+      return "error:divide by zero is not allowed"
+      return a/b
+      a=float(input("enter first number:"))
+      b=float(input("enter second number:"))
+
+print (f"addition:{add(a,b)}")
+print (f"subtraction:{sub(a,b)}")
+print (f"multiplication:{multiply(a,b)}")
+print (f"division:{divide(a,b)}")
